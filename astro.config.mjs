349234fcf80436ed10c528c7c0efc9036830dde9
Andrés Valenzuela <andres@valsis.cl>
2024-11-07 14:18:17 -0300

    Imágenes y contenidos (falta móvil y especificaciones).

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: "/aion-y",
-  site: "https://www.gacmotor.cl/aion-y",
+  base: "/empow",
+  site: "https://www.gacmotor.cl/empow",
 });
